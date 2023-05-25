8becc1f1f92e20bda5de7cbd9b09c5fae6c49e49
hafid <hafid.huesca@hotmail.com>
2023-05-25 09:20:32 -0500

    Listo para publicación

---
+++ script.js
@@ -0,0 +1,61 @@
+const textArea = document.querySelector(".text-area");
+const mensaje = document.querySelector(".mensaje");
+
+//La letra "e" es convertida para "enter"
+//La letra "i" es convertida para "imes"
+//La letra "a" es convertida para "ai"
+//La letra "o" es convertida para "ober"
+//La letra "u" es convertida para "ufat"
+
+function btnencriptar(){
+    const textoEncriptado = encriptar (textArea.value)
+    mensaje.value = textoEncriptado
+    textArea.value = "";
+    mensaje.style.backgroundImage="none"
+}
+
+function encriptar(stringEncriptado){
+    let matrizCodigo = [["e", "enter"], ["i","imes"], ["a","ai"], ["o","ober"], ["u","ufat"]];
+    stringEncriptado = stringEncriptado.toLowerCase()
+
+    for(let i = 0; i<matrizCodigo.length; i++){
+        if(stringEncriptado.includes(matrizCodigo[i][0])){
+            stringEncriptado = stringEncriptado.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])        
+        } 
+    }
+    return stringEncriptado
+}
+
+function btndesencriptar(){
+    const textEncriptado = desencriptar (textArea.value)
+    mensaje.value = textEncriptado
+    textArea.value = "";
+}
+
+function desencriptar(stringDesencriptado){
+    let matrizCodigo= [["e", "enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]];
+    stringDesencriptado = stringDesencriptado.toLowerCase()
+    
+    for(let i = 0; i<matrizCodigo.length; i++){
+        if(stringDesencriptado.includes(matrizCodigo[i][1])){
+            stringDesencriptado = stringDesencriptado.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0])        
+        } 
+    }
+    return stringDesencriptado
+}
+function copiarTexto() {
+    /* Obtén el elemento de la caja de texto */
+    var cajaTexto = document.getElementById('textArea')
+
+    /* Selecciona el texto de la caja de texto */
+    cajaTexto.select()
+
+    /* Copia el texto seleccionado al portapapeles */
+    document.execCommand("copy")
+
+    /* Deselecciona el texto */
+    cajaTexto.blur()
+
+    /* Opcional: muestra un mensaje de confirmación */
+    alert("¡Texto copiado al portapapeles!")
+}
